573107b9921013cd20cf9dccb6ddacadc78f97b5
TamaraBarusova <57990928+TamaraBarusova@users.noreply.github.com>
2023-03-13 15:56:22 +0100

    Add files via upload
    body A a B2, B3 z todo.txt DONE, zvysok in process

--- workscript/Script.R
+++ workscript/Script.R
@@ -1,8 +1,17 @@
+#### VOLBA PARAMETROV SKRIPTU ####
+rm(list = ls())
+args <- c("zzz.csv", "Infected", "InfPrior + VaccStatus")
+
+# args <- c("Input", "Outcome", "Covariates"), kde: 
+# 1. Input: zdrojovej csv soubor 
+# 2. Outcome: Infected nebo SeriousCovidProxy nebo LongCovid nebo Hospitalized 
+# 3. Covariates: InfPrior + VaccStatus nebo Immunity
+
 #### NACITANIE BALICKOV ####
 
 # Mena balickov
 packages <- c("readr", "tidyverse", "survival", "gtsummary", "expss", "plotrix", 
-              "gt", "forestmodel", "survminer", "webshot2", "ggstats")
+              "gt", "forestmodel", "survminer", "webshot2", "ggstats", "wesanderson")
 
 # Nainstalovanie doteraz nenainstalovanych balickov 
 installed_packages <- packages %in% rownames(installed.packages())
@@ -18,13 +27,22 @@ theme_gtsummary_journal("jama")
 
 #### NACITANIE DAT A POPISNA STATISTIKA ####
 
-rm(list = ls())
-data <- read_labelled_csv("zzz.csv")
+data <- read_labelled_csv(args[1])
+cox.f <- fread("cox_estimation_formulas.txt", stringsAsFactors = FALSE)
+f.input.outcome <- args[2]
+f.input.covariates <- args[3]
 
-head(data)
-names(data)
-str(data)
-summary(data)
+f.input <- cox.f[cox.f$Outcome == f.input.outcome 
+                 & cox.f$Covariates == f.input.covariates, 3] %>% 
+  as.character() %>% 
+  noquote() %>% 
+  as.formula()
+# f.input
+
+# head(data)
+# names(data)
+# str(data)
+# summary(data)
 
 # vytvorenie identifikátoru unikátnych pacientov 
 data <- 
@@ -48,10 +66,7 @@ sum_tab
 gt::gtsave(as_gt(sum_tab), file = "sum_tab.png")
 
 #### COXOV MODEL ####
-
-# m1_cox <- coxph(Surv(T1, T2, LongCovid) ~ InfPrior + VaccStatus + AgeGr + Sex,  data = data)
-
-m1_cox <- coxph(Surv(T1, T2, SeriousCovidProxy) ~ Immunity + AgeGr + Sex,  data = data)
+m1_cox <- coxph(f.input,  data = data)
 
 summary(m1_cox)
 
@@ -71,101 +86,108 @@ names(df)[c(3:4, 6:7)] <- c("HR_CI_lower", "HR_CI_upper","eff_CI_upper", "eff_CI
 
 #### GRAF - KRIVKY VYVANUTIA ####
 
+#### GRAF 2 ####
+# 2. VaccStatus: -full, -boost, -secboost, -partial
+
 # vyextrahovanie udajov potrebnych pre graf kriviek vyvanutia z tabulky df
 
-cas_early <- dimnames(df)[[1]][grep("InfPriorinf_EARLY", dimnames(df)[[1]])] %>% 
-  substring(19, 21) %>% as.numeric() %>% + 30
-cas_early
-
-cas_alpha <- dimnames(df)[[1]][grep("InfPriorinf_ALPHA", dimnames(df)[[1]])] %>% 
-  substring(19, 21) %>% as.numeric() %>% + 30
-cas_alpha
-
-cas_delta <- dimnames(df)[[1]][grep("InfPriorinf_DELTA", dimnames(df)[[1]])] %>% 
-  substring(19, 21) %>% as.numeric() %>% + 30
-cas_delta
-
-cas_omicron <- dimnames(df)[[1]][grep("InfPriorinf_OMICRON", dimnames(df)[[1]])] %>% 
-  substring(21, 23) %>% as.numeric() %>% + 30
-cas_omicron
-
-cas_vakcinace <- dimnames(df)[[1]][grep("VaccStatusfull", dimnames(df)[[1]])] %>% 
+cas_full <- dimnames(df)[[1]][grep("VaccStatusfull", dimnames(df)[[1]])] %>% 
   substring(16, 18) %>% as.numeric() %>% + 30
-cas_vakcinace
+# cas_full
 
-cas_booster <- dimnames(df)[[1]][grep("VaccStatusboost", dimnames(df)[[1]])] %>% 
+cas_boost <- dimnames(df)[[1]][grep("VaccStatusboost", dimnames(df)[[1]])] %>% 
   substring(17, 19) %>% as.numeric() %>% + 30
-cas_booster
+# cas_boost
+
+cas_secboost <- dimnames(df)[[1]][grep("VaccStatussecboost", dimnames(df)[[1]])] %>% 
+  substring(20, 22) %>% as.numeric() %>% + 30
+# cas_secboost
 
 cas_partial <- dimnames(df)[[1]][grep("VaccStatuspartial", dimnames(df)[[1]])] %>% 
   substring(19, 21) %>% as.numeric() %>% + 30
-cas_partial
+# cas_partial
 
-eff_early <- df$eff[grep("InfPriorinf_EARLY", dimnames(df)[[1]])]
-eff_alpha <- df$eff[grep("InfPriorinf_ALPHA", dimnames(df)[[1]])]
-eff_delta <- df$eff[grep("InfPriorinf_DELTA", dimnames(df)[[1]])]
-eff_omicron <- df$eff[grep("InfPriorinf_OMICRON", dimnames(df)[[1]])]
-eff_vakcinace <- df$eff[grep("VaccStatusfull", dimnames(df)[[1]])]
-eff_booster <- df$eff[grep("VaccStatusboost", dimnames(df)[[1]])]
+eff_full <- df$eff[grep("VaccStatusfull", dimnames(df)[[1]])]
+eff_boost <- df$eff[grep("VaccStatusboost", dimnames(df)[[1]])]
+eff_secboost <- df$eff[grep("VaccStatussecboost", dimnames(df)[[1]])]
 eff_partial <- df$eff[grep("VaccStatuspartial", dimnames(df)[[1]])]
 
-eff_CI_lower_early <- df$eff_CI_lower[grep("InfPriorinf_EARLY", dimnames(df)[[1]])]
-eff_CI_lower_alpha <- df$eff_CI_lower[grep("InfPriorinf_ALPHA", dimnames(df)[[1]])]
-eff_CI_lower_delta <- df$eff_CI_lower[grep("InfPriorinf_DELTA", dimnames(df)[[1]])]
-eff_CI_lower_omicron <- df$eff_CI_lower[grep("InfPriorinf_OMICRON", dimnames(df)[[1]])]
-eff_CI_lower_vakcinace <- df$eff_CI_lower[grep("VaccStatusfull", dimnames(df)[[1]])]
-eff_CI_lower_booster <- df$eff_CI_lower[grep("VaccStatusboost", dimnames(df)[[1]])]
+eff_CI_lower_full <- df$eff_CI_lower[grep("VaccStatusfull", dimnames(df)[[1]])]
+eff_CI_lower_boost <- df$eff_CI_lower[grep("VaccStatusboost", dimnames(df)[[1]])]
+eff_CI_lower_secboost <- df$eff_CI_lower[grep("VaccStatussecboost", dimnames(df)[[1]])]
 eff_CI_lower_partial <- df$eff_CI_lower[grep("VaccStatuspartial", dimnames(df)[[1]])]
 
-eff_CI_upper_early <- df$eff_CI_upper[grep("InfPriorinf_EARLY", dimnames(df)[[1]])]
-eff_CI_upper_alpha <- df$eff_CI_upper[grep("InfPriorinf_ALPHA", dimnames(df)[[1]])]
-eff_CI_upper_delta <- df$eff_CI_upper[grep("InfPriorinf_DELTA", dimnames(df)[[1]])]
-eff_CI_upper_omicron <- df$eff_CI_upper[grep("InfPriorinf_OMICRON", dimnames(df)[[1]])]
-eff_CI_upper_vakcinace <- df$eff_CI_upper[grep("VaccStatusfull", dimnames(df)[[1]])]
-eff_CI_upper_booster <- df$eff_CI_upper[grep("VaccStatusboost", dimnames(df)[[1]])]
+eff_CI_upper_full <- df$eff_CI_upper[grep("VaccStatusfull", dimnames(df)[[1]])]
+eff_CI_upper_boost <- df$eff_CI_upper[grep("VaccStatusboost", dimnames(df)[[1]])]
+eff_CI_upper_secboost <- df$eff_CI_upper[grep("VaccStatussecboost", dimnames(df)[[1]])]
 eff_CI_upper_partial <- df$eff_CI_upper[grep("VaccStatuspartial", dimnames(df)[[1]])]
 
-# graf / krivky vyvanutia 
-png(file = "graf_krivky_vyvanutia.png", width = 800, height = 500)
+png(file = "graf_krivky_vyvanutia_VaccStatus.png", width = 800, height = 500)
 par(mar = c(3.6, 3.6, 2, 1))
-plot(cas_early, eff_early, las = 1, xlim = c(0,1000), ylim = c(0, 1.1), xaxt = "n", 
+plot(cas_full, eff_full, las = 1, xlim = c(0,1000), ylim = c(0, 1.1), xaxt = "n", 
      xlab = "", ylab = "", type = "n")
 axis(1, at=seq(0, 1000, 90), seq(0, 1000, 90))
 abline(h = seq(0.2, 1, 0.2), col = "lightgray")
 
-plotCI(cas_early, eff_early, ui = eff_CI_upper_early, li = eff_CI_lower_early, 
+plotCI(cas_full, eff_full, ui = eff_CI_upper_full, li = eff_CI_lower_full, 
        add = T, pch = NA)
-plotCI(cas_alpha, eff_alpha, ui = eff_CI_upper_alpha, li = eff_CI_lower_alpha, 
+plotCI(cas_boost, eff_boost, ui = eff_CI_upper_boost, li = eff_CI_lower_boost, 
        add = T, pch = NA)
-plotCI(cas_delta, eff_delta, ui = eff_CI_upper_delta, li = eff_CI_lower_delta, 
-       add = T, pch = NA)
-plotCI(cas_omicron, eff_omicron, ui = eff_CI_upper_omicron, li = eff_CI_lower_omicron, 
-       add = T, pch = NA)
-plotCI(cas_vakcinace, eff_vakcinace, ui = eff_CI_upper_vakcinace, li = eff_CI_lower_vakcinace, 
-       add = T, pch = NA)
-plotCI(cas_booster, eff_booster, ui = eff_CI_upper_booster, li = eff_CI_lower_booster, 
+plotCI(cas_secboost, eff_secboost, ui = eff_CI_upper_secboost, li = eff_CI_lower_secboost, 
        add = T, pch = NA)
 plotCI(cas_partial, eff_partial, ui = eff_CI_upper_partial, li = eff_CI_lower_partial, 
        add = T, pch = NA)
 
+# terrain.colors(5)
+plot_color = wes_palette("Zissou1", 4, type = "continuous")
+lines(cas_full, eff_full, col = "#3B9AB2", type = "b", pch = 15, lwd = 2)
+lines(cas_boost, eff_boost, col = "#9EBE91", type = "b", pch = 16, lwd = 2)
+lines(cas_secboost, eff_secboost, col = "#E4B80E", type = "b", pch = 17, lwd = 2)
+lines(cas_partial, eff_partial, col = "#F21A00", type = "b", pch = 18, lwd = 2)
 
-lines(c(30, cas_early), c(1, eff_early), col = "red", type = "b", pch = 15, lwd = 2)
-lines(c(30, cas_delta), c(1, eff_delta), col = "green", type = "b", pch = 16, lwd = 2)
-lines(c(30, cas_alpha), c(1, eff_alpha), col = "blue", type = "b", pch = 14, lwd = 2)
-lines(c(30, cas_omicron), c(1, eff_omicron), col = "yellow", type = "b", pch = 13, lwd = 2)
-lines(cas_vakcinace, eff_vakcinace, col = "darkred", type = "b", pch = 17, lwd = 2)
-lines(cas_booster, eff_booster, col = "darkgreen", type = "b", pch = 18, lwd = 2)
-lines(cas_partial, eff_partial, col = "brown", type = "b", pch = 19, lwd = 2)
+mtext("Time from vaccination [days]", side = 1, line = 2.5) 
+mtext("Vaccine effectiveness", side = 2, line = 2.5) 
 
-mtext("Time from infection/vaccination [days]", side = 1, line = 2.5) 
-mtext("Effectiveness of immunity", side = 2, line = 2.5) 
-#title("Křivky vyvanutí")
+legend("topright", legend = c("Full vaccination", "Booster", "Second booster", "Partial vaccination"), 
+       col = plot_color,
+       lty = 1, pch = c(15, 16, 17, 18), lwd = 2, bty = "n")
 
-legend("bottomleft", legend = c("Early variants", "Alpha", "Delta", "Omicron", "Full vaccination", "Booster", "Partial vaccination"), col = c("red", "green", "blue", "yellow", "darkblue", "darkred", "darkgreen", "brown"), 
-       lty = 1, pch = c(15, 16, 14, 13, 17, 18, 19), lwd = 2, bty = "n")
+dev.off()
 
-# dev.copy nefunguje na serveru
-# dev.copy(device = png, filename = 'graf_krivky_vyvanutia.png', width = 800, height = 500)
+#### GRAF 3 ####
+# 3. InfPrior: inf_NA 
+
+cas_NA <- dimnames(df)[[1]][grep("InfPriorinf_NA", dimnames(df)[[1]])] %>% 
+  substring(16, 18) %>% as.numeric() %>% + 30 # + 15??
+# cas_NA
+
+eff_NA <- df$eff[grep("InfPriorinf_NA", dimnames(df)[[1]])]
+
+eff_CI_lower_NA <- df$eff_CI_lower[grep("InfPriorinf_NA", dimnames(df)[[1]])]
+
+eff_CI_upper_NA <- df$eff_CI_upper[grep("InfPriorinf_NA", dimnames(df)[[1]])]
+
+# graf / krivky vyvanutia 
+png(file = "graf_krivky_vyvanutia_InfPriorinf_NA.png", width = 800, height = 500)
+par(mar = c(3.6, 3.6, 2, 1))
+plot(cas_NA, eff_NA, las = 1, xlim = c(0, 1000), ylim = c(0, 1.1), xaxt = "n", 
+     xlab = "", ylab = "", type = "n")
+axis(1, at = seq(0, 1000, 90), seq(0, 1000, 90))
+abline(h = seq(0.2, 1, 0.2), col = "lightgray")
+
+plotCI(cas_NA, eff_NA, ui = eff_CI_upper_NA, li = eff_CI_lower_NA, 
+       add = T, pch = NA)
+
+plot_color = wes_palette("Zissou1", 1, type = "continuous")
+lines(cas_NA, eff_NA, col = plot_color, type = "b", pch = 15, lwd = 2)
+
+mtext("Time from infection [days]", side = 1, line = 2.5) 
+mtext("Effectiveness", side = 2, line = 2.5) 
+
+legend("topright", legend = c("InfPriorinf_NA"), # ?? 
+       col = plot_color,
+       lty = 1, pch = 15, lwd = 2, bty = "n")
+
 dev.off()
 
 #### TABULKY - HR a VE ####
@@ -190,8 +212,17 @@ forest_plot
 dev.off()
 
 # VE - vakcinacna efektivita
-m1_cox_VE_plot <- tbl_regression(m1_cox, include = c("InfPrior", "VaccStatus"), 
-                                 estimate_fun = function(x) {round(1 - exp(x), 2)}) %>% 
+
+if (f.input.covariates == "InfPrior + VaccStatus") {
+  include <- c("InfPrior", "VaccStatus") 
+  } else if (f.input.covariates == "Immunity") {
+    include <- f.input.covariates
+  } else {include <- NA
+    }
+
+m1_cox_VE_plot <- tbl_regression(m1_cox, include = include, 
+                                 estimate_fun = function(x) {round(1 - exp(x) , 2)}, 
+                                 conf.int = F) %>% 
   bold_p() %>% bold_labels() %>% 
   add_n(location = "level") %>%
   add_nevent(location = "level") %>%
