b57b62fecbf5bca53edf96f6eb5417217ab8b44e
TamaraBarusova <57990928+TamaraBarusova@users.noreply.github.com>
2023-03-14 07:25:01 +0100

    Add files via upload
    grifikyy done

--- workscript/Script.R
+++ workscript/Script.R
@@ -86,6 +86,95 @@ names(df)[c(3:4, 6:7)] <- c("HR_CI_lower", "HR_CI_upper","eff_CI_upper", "eff_CI
 
 #### GRAF - KRIVKY VYVANUTIA ####
 
+#### GRAF 1 ####
+# 1. Immunity: -inf, -full, -boost, -secboost, -hybridfull, -hybridboost
+dimnames(df)
+# vyextrahovanie udajov potrebnych pre graf kriviek vyvanutia z tabulky df
+
+cas_inf <- dimnames(df)[[1]][grep("Immunityinf", dimnames(df)[[1]])] %>% 
+  substring(13, 15) %>% as.numeric() %>% + 30
+# cas_full
+
+cas_full <- dimnames(df)[[1]][grep("Immunityfull", dimnames(df)[[1]])] %>% 
+  substring(14, 16) %>% as.numeric() %>% + 30
+# cas_boost
+
+cas_boost <- dimnames(df)[[1]][grep("Immunityboost", dimnames(df)[[1]])] %>% 
+  substring(15, 17) %>% as.numeric() %>% + 30
+# cas_secboost
+
+cas_secboost <- dimnames(df)[[1]][grep("Immunitysecboost", dimnames(df)[[1]])] %>% 
+  substring(18, 20) %>% as.numeric() %>% + 30
+# cas_partial
+
+cas_hybridfull <- dimnames(df)[[1]][grep("Immunityhybridfull", dimnames(df)[[1]])] %>% 
+  substring(20, 22) %>% as.numeric() %>% + 30
+# cas_partial
+
+cas_hybridboost <- dimnames(df)[[1]][grep("Immunityhybridboost", dimnames(df)[[1]])] %>% 
+  substring(21, 23) %>% as.numeric() %>% + 30
+# cas_partial
+
+eff_inf <- df$eff[grep("Immunityinf", dimnames(df)[[1]])]
+eff_full <- df$eff[grep("Immunityfull", dimnames(df)[[1]])]
+eff_boost <- df$eff[grep("Immunityboost", dimnames(df)[[1]])]
+eff_secboost <- df$eff[grep("Immunitysecboost", dimnames(df)[[1]])]
+eff_hybridfull <- df$eff[grep("Immunityhybridfull", dimnames(df)[[1]])]
+eff_hybridboost <- df$eff[grep("Immunityhybridboost", dimnames(df)[[1]])]
+
+eff_CI_lower_inf <- df$eff_CI_lower[grep("Immunityinf", dimnames(df)[[1]])]
+eff_CI_lower_full <- df$eff_CI_lower[grep("Immunityfull", dimnames(df)[[1]])]
+eff_CI_lower_boost <- df$eff_CI_lower[grep("Immunityboost", dimnames(df)[[1]])]
+eff_CI_lower_secboost <- df$eff_CI_lower[grep("Immunitysecboost", dimnames(df)[[1]])]
+eff_CI_lower_hybridfull <- df$eff_CI_lower[grep("Immunityhybridfull", dimnames(df)[[1]])]
+eff_CI_lower_hybridboost <- df$eff_CI_lower[grep("Immunityhybridboost", dimnames(df)[[1]])]
+
+eff_CI_upper_inf <- df$eff_CI_upper[grep("Immunityinf", dimnames(df)[[1]])]
+eff_CI_upper_full <- df$eff_CI_upper[grep("Immunityfull", dimnames(df)[[1]])]
+eff_CI_upper_boost <- df$eff_CI_upper[grep("Immunityboost", dimnames(df)[[1]])]
+eff_CI_upper_secboost <- df$eff_CI_upper[grep("Immunitysecboost", dimnames(df)[[1]])]
+eff_CI_upper_hybridfull <- df$eff_CI_upper[grep("Immunityhybridfull", dimnames(df)[[1]])]
+eff_CI_upper_hybridboost <- df$eff_CI_upper[grep("Immunityhybridboost", dimnames(df)[[1]])]
+
+png(file = "graf_krivky_vyvanutia_Immunity.png", width = 800, height = 500)
+par(mar = c(3.6, 3.6, 2, 1))
+plot(cas_inf, cas_inf, las = 1, xlim = c(0,1000), ylim = c(0, 1.1), xaxt = "n", 
+     xlab = "", ylab = "", type = "n")
+axis(1, at=seq(0, 1000, 90), seq(0, 1000, 90))
+abline(h = seq(0.2, 1, 0.2), col = "lightgray")
+
+plotCI(cas_inf, cas_inf, ui = eff_CI_upper_full, li = eff_CI_lower_full, 
+       add = T, pch = NA)
+plotCI(cas_full, eff_full, ui = eff_CI_upper_full, li = eff_CI_lower_full, 
+       add = T, pch = NA)
+plotCI(cas_boost, eff_boost, ui = eff_CI_upper_boost, li = eff_CI_lower_boost, 
+       add = T, pch = NA)
+plotCI(cas_secboost, eff_secboost, ui = eff_CI_upper_secboost, li = eff_CI_lower_secboost, 
+       add = T, pch = NA)
+plotCI(cas_hybridfull, eff_hybridfull, ui = eff_CI_upper_hybridfull, li = eff_CI_lower_hybridfull, 
+       add = T, pch = NA)
+plotCI(cas_hybridboost, eff_hybridboost, ui = eff_CI_upper_hybridboost, li = eff_CI_lower_hybridboost, 
+       add = T, pch = NA)
+
+# terrain.colors(5)
+plot_color = wes_palette("Zissou1", 6, type = "continuous")
+lines(cas_inf, eff_inf, col = "#3B9AB2", type = "b", pch = 15, lwd = 2)
+lines(cas_full, eff_full, col = "#6BB1C1", type = "b", pch = 16, lwd = 2)
+lines(cas_boost, eff_boost, col = "#BDC367", type = "b", pch = 17, lwd = 2)
+lines(cas_secboost, eff_secboost, col = "#E6C019", type = "b", pch = 18, lwd = 2)
+lines(cas_hybridfull, eff_hybridfull, col = "#E49100", type = "b", pch = 19, lwd = 2)
+lines(cas_hybridboost, eff_hybridboost, col = "#F21A00", type = "b", pch = 20, lwd = 2)
+
+mtext("Time from infection/vaccination [days]", side = 1, line = 2.5) #  lomitko je asi odveci
+mtext("Effectiveness", side = 2, line = 2.5) 
+
+legend("topright", legend = c("Infection", "Full vaccination", "Booster", "Second booster", 
+                              "Hybrid full", "Hybrid booster"), 
+       col = plot_color,
+       lty = 1, pch = (15:20), lwd = 2, bty = "n")
+
+dev.off()
+
 #### GRAF 2 ####
 # 2. VaccStatus: -full, -boost, -secboost, -partial
 
@@ -178,7 +267,7 @@ abline(h = seq(0.2, 1, 0.2), col = "lightgray")
 plotCI(cas_NA, eff_NA, ui = eff_CI_upper_NA, li = eff_CI_lower_NA, 
        add = T, pch = NA)
 
-plot_color = wes_palette("Zissou1", 1, type = "continuous")
+plot_color = wes_palette("Moonrise2", 1, type = "continuous")
 lines(cas_NA, eff_NA, col = plot_color, type = "b", pch = 15, lwd = 2)
 
 mtext("Time from infection [days]", side = 1, line = 2.5) 
@@ -189,6 +278,62 @@ legend("topright", legend = c("InfPriorinf_NA"), # ??
        lty = 1, pch = 15, lwd = 2, bty = "n")
 
 dev.off()
+
+#### GRAF 4 ####
+# 4.
+cas_alpha <- dimnames(df)[[1]][grep("InfPriorinf_ALPHA", dimnames(df)[[1]])] %>% 
+  substring(19, 21) %>% as.numeric() %>% + 30
+# cas_alpha
+
+cas_delta <- dimnames(df)[[1]][grep("InfPriorinf_DELTA", dimnames(df)[[1]])] %>% 
+  substring(19, 21) %>% as.numeric() %>% + 30
+# cas_delta
+
+cas_omicron <- dimnames(df)[[1]][grep("InfPriorinf_OMICRON", dimnames(df)[[1]])] %>% 
+  substring(21, 23) %>% as.numeric() %>% + 30
+# cas_omicron
+
+eff_alpha <- df$eff[grep("InfPriorinf_ALPHA", dimnames(df)[[1]])]
+eff_delta <- df$eff[grep("InfPriorinf_DELTA", dimnames(df)[[1]])]
+eff_omicron <- df$eff[grep("InfPriorinf_OMICRON", dimnames(df)[[1]])]
+
+eff_CI_lower_alpha <- df$eff_CI_lower[grep("InfPriorinf_ALPHA", dimnames(df)[[1]])]
+eff_CI_lower_delta <- df$eff_CI_lower[grep("InfPriorinf_DELTA", dimnames(df)[[1]])]
+eff_CI_lower_omicron <- df$eff_CI_lower[grep("InfPriorinf_OMICRON", dimnames(df)[[1]])]
+
+eff_CI_upper_alpha <- df$eff_CI_upper[grep("InfPriorinf_ALPHA", dimnames(df)[[1]])]
+eff_CI_upper_delta <- df$eff_CI_upper[grep("InfPriorinf_DELTA", dimnames(df)[[1]])]
+eff_CI_upper_omicron <- df$eff_CI_upper[grep("InfPriorinf_OMICRON", dimnames(df)[[1]])]
+
+# graf / krivky vyvanutia 
+png(file = "graf_krivky_vyvanutia_InfPrior.png", width = 800, height = 500)
+par(mar = c(3.6, 3.6, 2, 1))
+plot(cas_alpha, eff_alpha, las = 1, xlim = c(0,1000), ylim = c(0, 1.1), xaxt = "n", 
+     xlab = "", ylab = "", type = "n")
+axis(1, at=seq(0, 1000, 90), seq(0, 1000, 90))
+abline(h = seq(0.2, 1, 0.2), col = "lightgray")
+
+plotCI(cas_alpha, eff_alpha, ui = eff_CI_upper_alpha, li = eff_CI_lower_alpha, 
+       add = T, pch = NA)
+plotCI(cas_delta, eff_delta, ui = eff_CI_upper_delta, li = eff_CI_lower_delta, 
+       add = T, pch = NA)
+plotCI(cas_omicron, eff_omicron, ui = eff_CI_upper_omicron, li = eff_CI_lower_omicron, 
+       add = T, pch = NA)
+
+plot_color = wes_palette("FantasticFox1", 3, type = "continuous")
+lines(c(30, cas_delta), c(1, eff_delta), col = "#DD8D29", type = "b", pch = 16, lwd = 2)
+lines(c(30, cas_alpha), c(1, eff_alpha), col = "#46ACC8", type = "b", pch = 17, lwd = 2)
+lines(c(30, cas_omicron), c(1, eff_omicron), col = "#B40F20", type = "b", pch = 18, lwd = 2)
+
+mtext("Time from infection [days]", side = 1, line = 2.5) 
+mtext("Effectiveness", side = 2, line = 2.5) 
+
+legend("topright", legend = c("Alpha", "Delta", "Omicron"), 
+       col = plot_color, 
+       lty = 1, pch = c(16, 17, 18), lwd = 2, bty = "n")
+
+dev.off()
+
 
 #### TABULKY - HR a VE ####
 
